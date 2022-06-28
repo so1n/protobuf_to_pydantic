cb5514da21ca29131eaee16c3a894f41c487fefe
so1n <qaz6803609@163.com>
2022-06-28 15:05:18 +0800

    Modify, Feature, gen code support nested pydantic basemodel

--- example/simple_gen_code.py
+++ example/simple_gen_code.py
@@ -1,21 +1,17 @@
 from example.python_example_proto_code.example_proto.demo import demo_pb2
-from protobuf_to_pydantic import msg_to_pydantic_model
-from protobuf_to_pydantic.gen_code import pydantic_model_to_py_code
+from protobuf_to_pydantic import msg_to_pydantic_model, pydantic_model_to_py_file  # type: ignore
 
-print(
-    pydantic_model_to_py_code(
-        msg_to_pydantic_model(demo_pb2.NestedMessage),
-    )
+pydantic_model_to_py_file(
+    "./demo_gen_code.py",
+    msg_to_pydantic_model(demo_pb2.NestedMessage),
 )
-print(
-    pydantic_model_to_py_code(
-        msg_to_pydantic_model(demo_pb2.NestedMessage, parse_msg_desc_method=demo_pb2),
-    )
+pydantic_model_to_py_file(
+    "./demo_gen_code_by_pyi.py",
+    msg_to_pydantic_model(demo_pb2.NestedMessage, parse_msg_desc_method=demo_pb2),
 )
-print(
-    pydantic_model_to_py_code(
-        msg_to_pydantic_model(
-            demo_pb2.NestedMessage, parse_msg_desc_method="/home/so1n/github/protobuf_to_pydantic/example"
-        )
-    )
+pydantic_model_to_py_file(
+    "./demo_gen_code_by_protobuf_field.py",
+    msg_to_pydantic_model(
+        demo_pb2.NestedMessage, parse_msg_desc_method="/home/so1n/github/protobuf_to_pydantic/example"
+    ),
 )
